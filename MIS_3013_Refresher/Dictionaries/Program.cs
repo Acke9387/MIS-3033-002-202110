7031fb69b94b3e0b84b611b7542756f58716c455
Ackerman, Adam P <adam@ou.edu>
2021-08-30 11:46:33 -0500

    Finished the lists and dictionary problems

---
+++ MIS_3013_Refresher/Dictionaries/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> fruits = new Dictionary<string, double>();
+            fruits.Add("Apples", 0.99);
+            fruits.Add("Oranges", 0.50);
+            fruits.Add("Bananas", 0.50);
+            fruits.Add("Grapes", 2.99);
+            fruits.Add("Blueberries", 1.99);
+
+            Console.WriteLine("What fruit do you want a price of? >>");
+            string fruit = Console.ReadLine();
+
+            if (fruits.ContainsKey(fruit) == true)
+            {
+                Console.WriteLine($"{fruit}'s cost {fruits[fruit]}");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, we do not carry {fruit}'s.");
+            }
+
+
+        }
+    }
+}
---
+++ MIS_3013_Refresher/Lists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> examGrades = new List<double>();
+
+            bool shouldContinue = true;
+
+            while (shouldContinue)
+            {
+                Console.WriteLine("Please enter your exam grade >>");
+                double grade = Convert.ToDouble(Console.ReadLine());
+
+                examGrades.Add(grade);
+
+                Console.WriteLine("Do you want to enter another grade? yes or no >>");
+                string answer = Console.ReadLine();
+
+                if (answer.ToLower() == "no")
+                {
+                    //break;
+                    shouldContinue = false;
+                }
+            }
+
+            double avg, sum = 0;
+
+
+            foreach (double grade in examGrades)
+            {
+                sum += grade;
+            }
+
+            avg = sum / examGrades.Count;
+            Console.WriteLine($"The average of your exam grades is {avg.ToString("P2")}");
+        }
+    }
+}
