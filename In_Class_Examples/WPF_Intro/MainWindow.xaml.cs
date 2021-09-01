e085458d424ace9ff88629fd0d209256ba22c510
Ackerman, Adam P <adam@ou.edu>
2021-09-01 11:21:56 -0500

    Intro to WPF

---
+++ In_Class_Examples/WPF_Intro/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Intro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //first way
+            //lblOutput.Visibility = Visibility.Hidden;
+            lblOutput.Content = "";
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            int age = Convert.ToInt32(txtAge.Text);
+
+            DateTime today = DateTime.Now;
+            int birthYear = today.Year - age;
+
+            //MessageBox.Show($"You were born in {birthYear.ToString("G0")}");
+            lblOutput.Content = $"You were born in {birthYear.ToString("G0")}";
+            lblOutput.Visibility = Visibility.Visible;
+        }
+    }
+}
