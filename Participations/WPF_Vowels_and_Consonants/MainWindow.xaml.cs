2a3a3c61fb280ac640db3138d7e5107899a78a1a
Acke9387 <adam@ou.edu>
2021-09-08 11:46:43 -0500

    started wpf vowels and consonants

---
+++ Participations/WPF_Vowels_and_Consonants/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Vowels_and_Consonants
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //lstConsonants.Items.Add("W");
+            //lstsVowels.Items.Add('a');
+        }
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (char letter in txtWord.Text.ToLower())
+            {
+                if (letter == 'a' ||)
+                {
+
+                }
+
+
+            }
+        }
+    }
+}
