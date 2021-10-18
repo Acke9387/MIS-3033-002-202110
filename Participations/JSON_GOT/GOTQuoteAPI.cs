37bcae1ba5c24c5d751e2ccfbc76499640192773
Acke9387 <adam@ou.edu>
2021-10-18 10:49:08 -0500

    completed the GOT Quote participation and did an example of creating a new window

---
+++ Participations/JSON_GOT/GOTQuoteAPI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_GOT
+{
+    public class GOTQuoteAPI
+    {
+        public string quote {get;set;}
+        public string character { get; set; }
+
+        public GOTQuoteAPI()
+        {
+            quote = string.Empty;
+            character = string.Empty;
+        }
+
+    }
+}
---
+++ Participations/JSON_GOT/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_GOT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<GOTQuoteAPI> Quotes = new List<GOTQuoteAPI>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetQuote_Click(object sender, RoutedEventArgs e)
+        {
+            GOTQuoteAPI quote;
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync("https://got-quotes.herokuapp.com/quotes").Result;
+
+                quote = JsonConvert.DeserializeObject<GOTQuoteAPI>(json);
+
+            }
+
+            txtQuote.Text = quote.quote;
+            lblCharacter.Content = quote.character;
+            Quotes.Add(quote);
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            string json = JsonConvert.SerializeObject(Quotes);
+
+            File.WriteAllText("GOT_Quotes.json", json);
+
+            // Example of opening a new window
+            Example ex = new Example();
+            //ex.Quote = "";
+
+            ex.Show();
+            ex.ShowDialog();
+
+            //Process.Start("GOT_Quotes.json");
+        }
+    }
+}
