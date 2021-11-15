745c635d202db92ac258ff0c6a3a9179b800e3b1
Ackerman, Adam P <adamackrmn@gmail.com>
2021-11-15 11:43:25 -0600

    Did an MVC example project

---
+++ In_Class_Examples/MVC_Example1/Controllers/OUController.cs
@@ -0,0 +1,39 @@
+﻿using MVC_Example1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Example1.Controllers
+{
+    public class OUController : Controller
+    {
+        // GET: OU
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Students()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student()
+                {
+                    FirstName = "Micah",
+                    LastName = "Tison",
+                    SoonerID = 5
+                },
+                new Student()
+                {
+                    FirstName = "Talia",
+                    LastName = "Dean",
+                    SoonerID = 1
+                }
+            };
+
+            return View(students);
+        }
+    }
+}
